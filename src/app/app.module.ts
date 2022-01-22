85156310623feaddbed30dc989722e32a7aa8bba
jean pirul <jeanpirul@gmail.com>
2022-01-21 22:52:11 -0300

    tercer cambio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatSidenavModule} from '@angular/material/sidenav';
-import { ComponentsComponent } from './components/components.component';
 import { InitComponent } from './init/init.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentsComponent,
     InitComponent
   ],
   imports: [
